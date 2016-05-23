638753be11d35eaa262ac7cca2a234e1eb0ff6fc
patroclos_di <christoupat@gmail.com>
2016-05-23 19:22:22 +0300

    Fixed script

--- src/scripts/sentence_generator.py
+++ src/scripts/sentence_generator.py
@@ -2,7 +2,7 @@
 import random
 
 s_nouns = ["A dude", "My mom", "The killer", "Some good guy", "A cat with rabies", "A thief", "Your homie", "This cool guy my gardener met yesterday", "Superman"]
-p_verbs = ["abort", "betray", "crash", ,"kill","admire", "bonus", "calm", "good","accept","happiness","amazing","sad"]
+s_verbs = ["abort", "betray", "crash","kill","admire", "bonus", "calm", "good","accept","happiness","amazing","sad"]
 infinitives = ["to make a pie.", "for a bad reason.", "because the sky is green.", "for a disease.", "to be able to make toast explode.", "to know more about archeology."]
 
 def generate():
